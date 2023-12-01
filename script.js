6ce983646af48dfa91087321d2e400ca023abda0
CodingMayus <106496765+CodingMayus@users.noreply.github.com>
2023-12-01 12:24:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 window.addEventListener('scroll', () => {
   document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
+ var txts=["I am an International Baccalaureate Student", "I am to code","I am a Mathematician"," I am a runner","I am competitive"," I am working towards my dreams!"];
+ 
+ document.getElementById("clicky").addEventListener("Click", typeWriter);
+function typeWriter() {
+var speed = 100;
+var cnt =0;
+/* for(cnt =0;cnt<6;cnt++){ */
+//document.getElementById("attributes").innerHTML = "";
+var i =0;
+  if (i < txts[1].length) {
+    document.getElementById("attributes").innerHTML += txts[1].charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+/* if(i>=0){
+   document.getElementById("attributes").innerHTML -= txts[cnt].charAt(i);
+ } */
+//}
+
+} 
